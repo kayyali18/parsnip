51ee27cf2ed1fc568548eb87aa63888a6e71e340
Ahmad Kayyali <kayyali18@gmail.com>
2019-01-25 15:38:29 -0500

    Add react-redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import tasks from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import "./index.css";
+
+const store = createStore(tasks);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
