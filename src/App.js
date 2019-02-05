4c064ba8eabce9984c2fd972ff46d73a1a71145d
Ahmad Kayyali <kayyali18@gmail.com>
2019-02-05 17:10:19 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import TasksPage from "./components/TasksPage";
+import { createTask } from "./actions";
 
 class App extends Component {
+  onCreateTask = ({ title, description }) => {
+    this.props.dispatch(createTask({ title, description }));
+  };
+
   render() {
     return (
       <div className="main-content">
-        <TasksPage tasks={mockTasks} />
+        <TasksPage tasks={this.props.tasks} onCreateTask={this.onCreateTask} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ tasks: state.tasks });
+
+export default connect(mapStateToProps)(App);
+
+// A side effect is any code that has a noticeable
+// effect on the outside world.
+// Such as writing to a disk or mutating data. Put another way,
+// it is code that does anything but take inputs and return a result
+
+// Deterministic == opposite of side effect
